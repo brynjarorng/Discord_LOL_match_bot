646a03079b4d42bf0bbd024701484360adf967bd
brynjarorng <brynjarorng@gmail.com>
2020-09-15 12:02:57 +0200

    added sql table for player matches

---
+++ create_db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE player_matches (
+    player_gameId  bigint,
+    player_role text,
+    player_season int,
+    player_platformId text,
+    player_champion int,
+    player_queue int,
+    player_lane text,
+    player_timestamp bigint
+);
