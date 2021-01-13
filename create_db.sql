9a310a2016e856255ce6ecda2cc62595e4f301ae
brynjarorng <brynjarorng@gmail.com>
2021-01-13 15:16:17 +0000

    db changes

--- create_db.sql
+++ create_db.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS player_matches;
 CREATE TABLE player_matches (
-    player_gameId  bigint,
+    player_gameId bigint,
     player_role text,
     player_season int,
     player_platformId text,
@@ -12,3 +12,11 @@ CREATE TABLE player_matches (
 );
 
 -- NEED TABLE TO LINK TO SUMMONER
+DROP TABLE IF EXISTS summoner_info;
+CREATE TABLE summoner_info (
+    encrypted_account_id text,
+    accountId text,
+    puuid text,
+    name text,
+    summonerLevel int
+)
